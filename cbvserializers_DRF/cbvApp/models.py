e720297647fd11ae46719dd9bb3adf144ab5d69b
Aranya Majumdar <74165561+aranya-code@users.noreply.github.com>
2026-03-08 16:34:05 +0530

    add students project

---
+++ cbvserializers_DRF/cbvApp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Student(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100, null=True, default='Aranya')
+    score = models.IntegerField()
+
+    def __str__(self):
+        return self.name
---
+++ cbvserializers_DRF/cbvApp/views.py
@@ -0,0 +1,81 @@
+from django.shortcuts import render
+from cbvApp.models import Student
+from cbvApp.serializers import StudentSerializers
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+from rest_framework import generics, mixins
+
+class StudentList(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializers
+
+    def get(self, request):
+        return self.list(request)
+    
+    def post(self, request):
+        return self.create(request)
+    
+class StudentDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, 
+                    mixins.DestroyModelMixin,generics.GenericAPIView):
+    
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializers
+
+    def get(self, request, pk):
+        return self.retrieve(request, pk)
+    
+    def put(self, request, pk):
+        return self.update(request, pk)
+    
+    def delete(self, request, pk):
+        return self.destroy(request, pk)
+
+
+
+
+"""
+
+class StudentList(APIView):
+
+    def get(self, request):
+        students = Student.objects.all()
+        serializer = StudentSerializers(students, many = True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = StudentSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+
+class StudentDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Student.objects.get(pk=pk)
+        except Student.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        student = self.get_object(pk)
+        serializer = StudentSerializers(student)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        student = self.get_object(pk)
+        serializer = StudentSerializers(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        student = self.get_object(pk)
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+"""
