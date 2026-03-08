32e7a6c88834125ac028f32662b6e11640776d0a
Aranya Majumdar <74165561+aranya-code@users.noreply.github.com>
2026-03-08 18:10:14 +0530

    add comments

--- fvbPassengers_DRF/passengers/serializers.py
+++ fvbPassengers_DRF/passengers/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from passengers.models import Passengers
 
 class PassengerSerializer(serializers.ModelSerializer):
+    """
+    Translates the Passenger model instances into JSON format (and vice versa).
+    """
+    # Serializing the model using ModelSerializer
     class Meta:
         model = Passengers
         fields = ['id', 'name','travelpoints']
--- fvbPassengers_DRF/passengers/views.py
+++ fvbPassengers_DRF/passengers/views.py
@@ -7,12 +7,14 @@ from rest_framework.decorators import api_view
 
 @api_view(['GET', 'POST'])
 def passenger_list(request):
-    
+
+    # Retrieve all the Passengers    
     if request.method == 'GET':
         all_passenger = Passengers.objects.all()
         serializer = PassengerSerializer(all_passenger, many= True)
         return Response(serializer.data)
     
+    # Create a Passenger
     elif request.method == 'POST':
         serializer = PassengerSerializer(data = request.data)
         if serializer.is_valid():
@@ -22,15 +24,18 @@ def passenger_list(request):
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def passenger_detail(request, pk):
+    # Getting Passenger detail by primary key
     try:
         passenger = Passengers.objects.get(pk=pk)
     except Passengers.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
     
+    # Retreive data
     if request.method =='GET':
         serializer = PassengerSerializer(passenger)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
+    # Update data
     elif request.method == 'PUT':
         serializer=PassengerSerializer(passenger,data = request.data)
         if serializer.is_valid():
@@ -38,6 +43,7 @@ def passenger_detail(request, pk):
             return Response(serializer.data, status= status.HTTP_201_CREATED)
         return Response (serializer.errors, status= status.HTTP_400_BAD_REQUEST)
     
+    # Delete data
     elif request.method == 'DELETE':
         passenger.delete()
         return Response (status=status.HTTP_204_NO_CONTENT)
