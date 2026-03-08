bcf2ee1b72149246e4c8cdd1cfd2da1434ee98df
Aranya Majumdar <74165561+aranya-code@users.noreply.github.com>
2026-03-08 18:19:42 +0530

    add comments

--- Employees_DRF/firstApp/models.py
+++ Employees_DRF/firstApp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class emp(models.Model):
+    # Creating model for employees
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
     salary = models.IntegerField()
--- Employees_DRF/firstApp/urls.py
+++ Employees_DRF/firstApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from firstApp import views
 
+# Creating a url path for employees list.
 urlpatterns = [
     path('emp/', views.employeeView, name= 'employee_list'),
 ]
